7bc8d7cd5ea19ace88d66275fb3cd7fa67257988
NdeyeThiane <ndeyet2004@gmail.com>
2024-10-28 14:44:02 -0700

    redux courseSlice
    create this branch to work on my courseSlice feature

--- client/src/app/features/course/courseSlice.js
+++ client/src/app/features/course/courseSlice.js
@@ -39,45 +39,6 @@ export const courseSlice = createSlice(
 )
 
 
-// export const getTasks = createAsyncThunk('course/tasks', async() =>{
-//     const response = await fetch("http://localhost:3000/")
-//     return response.json();
-    
-// })
-
-// export const getTasks = createSlice(
-//     {
-//         name:'tasks',
-//         extraReducers: (builder) =>{
-//             builder.addCase(getCourses.pending, (state, action) =>{
-//                 state.loading = true;
-//             }),
-//             builder.addCase(getCourses.fulfilled, (state, action) =>{
-//                 state.loading = false;
-//                 state.courses = action.payload;
-//             }),
-//             builder.addCase(getCourses.rejected, (state, action) =>{
-//                 state.error = true;
-//             })
-//         }
-           
-
-            
-        
-//     }
-// )
-
-
-
-// deleteCourse:(state, action)=>{
-
-// },
-
-// editCourse:(state, action)=>{
-
-// },
-// getCourse: (state, action)=>{
-
 
 
 export default courseSlice.reducer;
